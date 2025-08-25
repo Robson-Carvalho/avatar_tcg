a7f8ebb655f715b58f88422fa6614543b0762471
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-08-25 18:43:37 -0300

    feat: adiciona classe da partida

---
+++ src/main/java/com/oak/avatar_tcg/game/Match.java
@@ -0,0 +1,55 @@
+package com.oak.avatar_tcg.game;
+
+import com.oak.avatar_tcg.model.Card;
+import com.oak.avatar_tcg.model.Deck;
+import com.oak.http.WebSocket;
+
+import javax.swing.plaf.PanelUI;
+import java.util.List;
+import java.util.UUID;
+
+public class Match {
+    private String id;
+    private String PlayerOneID;
+    private String PlayerTwoID;
+    private WebSocket SocketPlayerOne;
+    private WebSocket SocketPlayerTwo;
+    private List<Card> playerOneCards;
+    private List<Card> playerTwoCards;
+
+    public Match(String PlayerOneID, WebSocket SocketPlayerOne, String PlayerTwoID, WebSocket SocketPlayerTwo) {
+        this.id = UUID.randomUUID().toString();
+        this.PlayerOneID = PlayerOneID;
+        this.PlayerTwoID = PlayerTwoID;
+        this.SocketPlayerOne = SocketPlayerOne;
+        this.SocketPlayerTwo = SocketPlayerTwo;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getPlayerOneID() {
+        return this.PlayerOneID;
+    }
+
+    public String getPlayerTwoID() {
+        return this.PlayerTwoID;
+    }
+
+    public WebSocket getSocketPlayerOne() {
+        return this.SocketPlayerOne;
+    }
+
+    public WebSocket getSocketPlayerTwo() {
+        return this.SocketPlayerTwo;
+    }
+
+    public void battle(){
+        // resolver jogo
+    }
+
+    public void getStateMatch(){
+        // retornar JSON do estado atual
+    }
+}
