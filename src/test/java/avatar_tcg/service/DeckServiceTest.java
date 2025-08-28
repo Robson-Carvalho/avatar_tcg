6755dd265211a75fa005434377eed3f4278888b1
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2025-08-28 16:35:17 -0300

    test: adiciona arquivos de teste

---
+++ src/test/java/avatar_tcg/service/DeckServiceTest.java
@@ -0,0 +1,14 @@
+package avatar_tcg.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeckServiceTest {
+
+    @Test
+    public void testSoma() {
+        int resultado = 5;
+        assertEquals(5, resultado);
+    }
+}
